3a986b0f8e2e17f81b3848dec8489e265d807b91
gusdev0258 <16326176760@edu.udesc.br>
2023-03-24 23:48:46 -0300

    add: Created ParkingSpotEntity on model

--- src/main/java/com/api/parkingcontrol1/ParkingControl1Application.java
+++ src/main/java/com/api/parkingcontrol1/ParkingControl1Application.java
@@ -2,12 +2,20 @@ package com.api.parkingcontrol1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControl1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingControl1Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "<h1>Hello World</h1>";
     }
 
 }
