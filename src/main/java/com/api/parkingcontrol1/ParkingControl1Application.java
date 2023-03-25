e0fbc73524fbb3f8349322d365cd789b45f14aa1
gusdev0258 <16326176760@edu.udesc.br>
2023-03-24 22:59:38 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol1/ParkingControl1Application.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControl1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingControl1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/parkingcontrol1/ParkingControl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControl1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
